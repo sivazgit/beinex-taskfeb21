f3e44738899d3ba61baa83cb83206fa4ea4361bd
unknown <sivapriya3256@gmail.com>
2023-02-22 09:11:32 +0530

    feb21

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path:'',component:HomeComponent
   },
-  {
-    path:'d'
-  }
+{
+ path:'database2',component:DashboardComponent
+}
 ];
 
 @NgModule({
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -8,10 +8,25 @@ import { DataServiceService } from 'src/app/services/data-service.service';
 })
 export class DashboardComponent {
 
+  public pstDetails:any;
+  public viewOrHide:boolean = false;
+
   constructor(private ds:DataServiceService){}
+
+  ngOnInit(): void{
+    this.getPstDetails();
+  }
 
 
   getPstDetails(){
-    
+    this.ds.getPstDetails().subscribe(response=>{
+      console.log('response',response);
+      this.pstDetails = response;
+      
+    })
+  }
+
+  viewData(){
+    this.viewOrHide = !this.viewOrHide;
   }
 }
